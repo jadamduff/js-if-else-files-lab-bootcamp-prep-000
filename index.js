0d988dba077098c88bf4d111ee914a705da38255
Adam Duff <j.adam.duff@gmail.com>
2018-08-07 18:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name === "Susan") {
   console.log("The name is correct")
 }
 else {
-  console.log("Expected name to be Susan")
+  console.log("Expected the name to be Susan")
 }
 
 // Don't worry about this
